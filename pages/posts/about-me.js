24223893fcf3fbb3516cafd79429170427014f92
Nehemiah Wilson <nehemiahwilson@MacBook-Pro-82.local>
2020-09-11 10:48:53 -0400

    Replace old jpgs with compressed jpgs

--- pages/posts/about-me.js
+++ pages/posts/about-me.js
@@ -1,8 +1,8 @@
 export default function AboutMe() {
     return (
         <div id="page-div">
-            <img class="page-photo" src="../DSC_0038.jpg"/>
-            <img class="page-photo" src="../DSC_0041.jpg"/>
+            <img class="page-photo" src="../compressed/DSC_0038-min.jpg"/>
+            <img class="page-photo" src="../compressed/DSC_0041-min.jpg"/>
             <p class="page-text">I have an insatiable desire to impact lives in a positive manner through my passion for computer science and digital media, which developed from teaching my high school's computer programming class during my 11th and 12th grade years. I am pursuing a degree in Computational Media with a concentration in UI/UX Design. My goal is to become a UI/UX Designer in a role that allows me to combine my technical and creative skills to solve the most pressing problems in modern technology.</p>
             <a class="back-button" href="/">BACK TO HOME</a>
         </div>
